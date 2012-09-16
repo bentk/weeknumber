2d80dfa4250c6000b7be9fe41727a9d63be17d0b
Bent Kristiansen <bent.kristiansen@gmail.com>
2012-09-16 15:15:43 +0200

    update format

--- week/TileSquareBlock.aspx.cs
+++ week/TileSquareBlock.aspx.cs
@@ -7,14 +7,7 @@ namespace week
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write(string.Format(@"<tile>
-                              <visual>
-                                <binding template='TileSquareBlock'>
-                                  <text id='1'>{0}</text>
-                                  <text id='2'>{1}</text>
-                                </binding>  
-                              </visual>
-                            </tile>",
+            Response.Write(string.Format(@"<tile><visual><binding template='TileSquareBlock'><text id='1'>{0}</text><text id='2'>{1}</text></binding></visual></tile>",
                             DateTimeFormatInfo.CurrentInfo.Calendar.GetWeekOfYear(DateTime.Today,DateTimeFormatInfo.CurrentInfo.CalendarWeekRule, DateTimeFormatInfo.CurrentInfo.FirstDayOfWeek),
                             DateTime.Today.ToShortDateString()));
         }
