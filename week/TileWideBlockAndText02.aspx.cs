59873d0638ef8d40862d7c7987ad1e489e3eab4f
Bent Kristiansen <bent.kristiansen@gmail.com>
2012-09-18 14:19:39 +0200

    s

--- week/TileWideBlockAndText02.aspx.cs
+++ week/TileWideBlockAndText02.aspx.cs
@@ -13,7 +13,7 @@ namespace week
 
             Response.Write(string.Format(@"<tile><visual branding='name'>
                 <binding template='TileWideBlockAndText02'><text id='1'>{0}</text><text id='2'>{1}</text><text id='3'>{2}</text></binding>
-                <binding template='TileSquareBlock'><text id='1'>{0}</text><text id='2'>{1}</text></binding></visual></tile>",
+                <binding template='TileSquareBlock'><text id='1'>{1}</text><text id='2'>{2}</text></binding></visual></tile>",
                 "",       
                 a.Calendar.GetWeekOfYear(DateTime.Today, a.DateTimeFormat.CalendarWeekRule, a.DateTimeFormat.FirstDayOfWeek),
                             DateTime.Today.Day + ". " + a.DateTimeFormat.GetMonthName(DateTime.Today.Month)));
