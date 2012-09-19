57d8e26a519104d8bc09c7ee81dd2ee4de5b367a
Bent Kristiansen <bent.kristiansen@gmail.com>
2012-09-19 22:29:02 +0200

    fixed date format

--- TileXmlHelper/TileXml.cs
+++ TileXmlHelper/TileXml.cs
@@ -19,9 +19,9 @@ namespace TileXmlHelper
             return string.Format(@"<tile><visual branding='logo'>
                 <binding template='TileWideBlockAndText02'><text id='1'>{0}</text><text id='2'>{1}</text><text id='3'></text></binding>
                 <binding template='TileSquareBlock'><text id='1'>{1}</text><text id='2'>{2}</text></binding></visual></tile>",
-                DateTime.Today.ToString(ci.DateTimeFormat.LongDatePattern),       
+                DateTime.Today.ToString("D", ci.DateTimeFormat),       
                 ci.Calendar.GetWeekOfYear(DateTime.Today, ci.DateTimeFormat.CalendarWeekRule, ci.DateTimeFormat.FirstDayOfWeek),
-                            DateTime.Today.ToString(ci.DateTimeFormat.MonthDayPattern));
+                            DateTime.Today.ToString(ci.DateTimeFormat.MonthDayPattern, ci.DateTimeFormat));
 
         }
     }
