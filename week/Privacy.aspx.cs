1bb943372ef7fa28bb8881295d9a509b55126008
Bent Kristiansen <bent.kristiansen@gmail.com>
2012-10-05 10:23:23 +0200

    s

--- week/Privacy.aspx.cs
+++ week/Privacy.aspx.cs
@@ -8,11 +8,20 @@ namespace week
         protected void Page_Load(object sender, EventArgs e)
         {
             var culture = Request.Params["culture"] ?? Request.Params["language"] ?? Request.Params["lang"] ?? "en-US";
-            var a = new CultureInfo(culture);
+            CultureInfo a;
+            try
+            {
+                a = new CultureInfo(culture);
+            }
+            catch (Exception)
+            {
+                a = new CultureInfo("en-US");
+            }
+            
 
             var text = "This application does not share or store any statistics about you or your usage.";
 
-            if (a.Name.ToLower() == "no")
+            if (a.Name.ToLower() == "nb" || a.Name.ToLower() == "nn")
                 text = "Denne appen hverken videresender eller lagrer informasjon om deg eller ditt bruksmønster";
 
             Response.Write(string.Format(@"<html><body><h2>Week Number</h2><p>{0}</p></body></html>",text));
